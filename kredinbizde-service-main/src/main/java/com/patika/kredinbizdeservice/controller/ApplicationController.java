b7575fcfbdc2415bf1c23b240cecc2daf8257ec2
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-08 00:54:34 +0300

    kafka working:kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/controller/ApplicationController.java

--- kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/controller/ApplicationController.java
+++ kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/controller/ApplicationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/applications")
+@RequestMapping("api/v1/application")
 @RequiredArgsConstructor
 public class ApplicationController {
 
