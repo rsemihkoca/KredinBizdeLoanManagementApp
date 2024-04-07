3f80174eeb5c0c5bdcc52ab817841cc07f27e3aa
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-08 00:54:34 +0300

    kafka working:kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/controller/UserController.java

--- kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/controller/UserController.java
+++ kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/controller/UserController.java
@@ -11,7 +11,7 @@ import java.io.FileNotFoundException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
 public class UserController {
 
