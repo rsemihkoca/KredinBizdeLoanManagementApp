7bbc65673d672ee6046ef0e215fc630d8536cbd4
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-08 00:54:34 +0300

    kafka working:gateway-service-main/src/main/java/com/rsemihkoca/gatewayservicemain/configuration/GatewayConfig.java

--- gateway-service-main/src/main/java/com/rsemihkoca/gatewayservicemain/configuration/GatewayConfig.java
+++ gateway-service-main/src/main/java/com/rsemihkoca/gatewayservicemain/configuration/GatewayConfig.java
@@ -1,2 +1,25 @@
-package com.rsemihkoca.gatewayservicemain.configuration;public class GatewayConfig {
+package com.rsemihkoca.gatewayservicemain.configuration;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("user-service-main", r -> r
+                        .path("/api/v1/user/**")
+                        .uri("lb://USER-SERVICE-MAIN")) //.uri("http://localhost:8081"))
+                .route("user-service", r -> r
+                        .path("/api/v1/users/**")
+                        .uri("lb://USER-SERVICE-MAIN")) //.uri("http://localhost:8081"))
+                .route("fallback", r -> r
+                        .path("/**")
+                        .uri("forward:/error"))
+                .build();
+    }
 }
