b628e19bd76847c918ff8da9a9a64c23d45192d6
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-08 00:54:34 +0300

    kafka working:gateway-service-main/src/main/java/com/rsemihkoca/gatewayservicemain/Application.java

--- gateway-service-main/src/main/java/com/rsemihkoca/gatewayservicemain/Application.java
+++ gateway-service-main/src/main/java/com/rsemihkoca/gatewayservicemain/Application.java
@@ -3,13 +3,17 @@ package com.rsemihkoca.gatewayservicemain;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
+// tag::code[]
 @SpringBootApplication
 @EnableDiscoveryClient
-public class GatewayServiceMainApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GatewayServiceMainApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
