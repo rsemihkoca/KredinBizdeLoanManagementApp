80471b52815fc865f7382c63c8840501ebe8af4b
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-09 00:24:10 +0300

    application service init:application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/producer/TransactionProducer.java

--- application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/producer/TransactionProducer.java
+++ application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/producer/TransactionProducer.java
@@ -2,7 +2,7 @@ package com.rsemihkoca.applicationservicemain.producer;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.rsemihkoca.userservicemain.producer.dto.Transaction;
+import com.rsemihkoca.applicationservicemain.producer.dto.Transaction;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
