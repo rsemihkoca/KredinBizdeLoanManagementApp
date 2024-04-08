4b2f39cb98dfba607783743d09bf32567081e1ee
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-08 18:27:59 +0300

    user service ok:user-service-main/src/main/java/com/rsemihkoca/userservicemain/service/UserService.java

--- user-service-main/src/main/java/com/rsemihkoca/userservicemain/service/UserService.java
+++ user-service-main/src/main/java/com/rsemihkoca/userservicemain/service/UserService.java
@@ -2,9 +2,12 @@ package com.rsemihkoca.userservicemain.service;
 
 import java.util.List;
 
+import com.rsemihkoca.userservicemain.exceptions.dto.request.CreateUserRequest;
+import com.rsemihkoca.userservicemain.exceptions.dto.response.UserResponse;
 import com.rsemihkoca.userservicemain.model.User;
 import com.rsemihkoca.userservicemain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,22 +15,29 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService {
 
+    private final ModelMapper modelMapper;
     private UserRepository userRepository;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(ModelMapper modelMapper, UserRepository userRepository) {
+        this.modelMapper = modelMapper;
         this.userRepository = userRepository;
     }
 
-    public User createUser(User user) {
-        return userRepository.save(user);
+    public UserResponse createUser(CreateUserRequest user) {
+        User userEntity = modelMapper.map(user, User.class);
+        User savedUser = userRepository.save(userEntity);
+        return modelMapper.map(savedUser, UserResponse.class);
     }
 
-    public User getById(String userId) {
-        return userRepository.getUserById(userId);
+    public UserResponse getById(Long userId) {
+        User getUser = userRepository.getUserByUserId(userId);
+        return modelMapper.map(getUser, UserResponse.class);
     }
 
-    public List<User> getAll() {
-        return userRepository.getAll();
+    public List<UserResponse> getAll() {
+        List<User> users = userRepository.findAll();
+        return users.stream().map(user -> modelMapper.map(user, UserResponse.class)).toList();
+
     }
 }
