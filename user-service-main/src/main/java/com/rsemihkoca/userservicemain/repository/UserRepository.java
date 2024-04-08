f82ba8e1e5f9d06e9de78c08d713e38f1e31e335
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-08 18:27:59 +0300

    user service ok:user-service-main/src/main/java/com/rsemihkoca/userservicemain/repository/UserRepository.java

--- user-service-main/src/main/java/com/rsemihkoca/userservicemain/repository/UserRepository.java
+++ user-service-main/src/main/java/com/rsemihkoca/userservicemain/repository/UserRepository.java
@@ -1,35 +1,12 @@
 package com.rsemihkoca.userservicemain.repository;
 
 import com.rsemihkoca.userservicemain.model.*;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Repository
-public class UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
 
-    private final List<User> userList = new ArrayList<>();
-
-    public User save(User user) {
-        userList.add(user);
-        return user;
-    }
-
-    public User getUserById(String userId) {
-        return userList.stream().filter(user -> user.getUserId().equals(userId)).findFirst().orElse(null);
-    }
-
-    public List<User> getAll() {
-        return userList;
-    }
-
-    public void initializeDatabase() {
-        userList.add(new User("1", "Cem", 30));
-        userList.add(new User("2", "Cem", 25));
-        userList.add(new User("3", "Cem", 28));
-        userList.add(new User("4", "Cem", 27));
-        userList.add(new User("5", "Semih", 24)); 
-    }
+    User getUserByUserId(Long userId);
 
 }
