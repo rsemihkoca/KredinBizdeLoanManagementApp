dd221570fee2ea35ffa22c6bd9938076d35d943b
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-09 00:24:10 +0300

    application service init:user-service-main/src/main/java/com/rsemihkoca/userservicemain/exceptions/dto/request/CreateUserRequest.java

--- user-service-main/src/main/java/com/rsemihkoca/userservicemain/exceptions/dto/request/CreateUserRequest.java
+++ user-service-main/src/main/java/com/rsemihkoca/userservicemain/exceptions/dto/request/CreateUserRequest.java
@@ -6,10 +6,12 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @AllArgsConstructor
 @Data
+@Builder
 public class CreateUserRequest {
 
     @NotBlank(message = "Name cannot be blank")
