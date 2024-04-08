c96904398e3b737f6712847743b38a9b8909bd97
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-08 21:33:07 +0300

    userservice finished:user-service-main/src/main/java/com/rsemihkoca/userservicemain/configuration/BeanConfiguration.java

--- user-service-main/src/main/java/com/rsemihkoca/userservicemain/configuration/BeanConfiguration.java
+++ user-service-main/src/main/java/com/rsemihkoca/userservicemain/configuration/BeanConfiguration.java
@@ -1,15 +1,35 @@
 package com.rsemihkoca.userservicemain.configuration;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.modelmapper.config.Configuration.*;
+import java.time.Duration;
 
 @Configuration
 public class BeanConfiguration {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper mapper = new ModelMapper();
+
+        mapper.getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(AccessLevel.PRIVATE)
+                .setMatchingStrategy(MatchingStrategies.STANDARD);
+
+        return mapper;
     }
 
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(5))
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+    }
 }
