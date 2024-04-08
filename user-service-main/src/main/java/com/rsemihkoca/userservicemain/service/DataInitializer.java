ab1a72a0d02e41c5f05abac1aa5489e0cd777788
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-08 18:27:59 +0300

    user service ok:user-service-main/src/main/java/com/rsemihkoca/userservicemain/service/DataInitializer.java

--- user-service-main/src/main/java/com/rsemihkoca/userservicemain/service/DataInitializer.java
+++ user-service-main/src/main/java/com/rsemihkoca/userservicemain/service/DataInitializer.java
@@ -1,2 +1,98 @@
-package com.rsemihkoca.userservicemain.service;public class DataInitializer {
+package com.rsemihkoca.userservicemain.service;
+
+import com.rsemihkoca.userservicemain.model.*;
+import com.rsemihkoca.userservicemain.repository.UserRepository;
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+@Component
+@Log
+public class DataInitializer implements CommandLineRunner {
+
+    private final Logger logger = Logger.getLogger(DataInitializer.class.getName());
+
+    private final UserRepository userRepository;
+
+    public DataInitializer(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public void run(String... args) {
+
+        List<User> users = userRepository.findAll();
+        if (!users.isEmpty()) {
+            logger.info("Data already initialized");
+            return;
+        } else {
+            logger.info("Data is empty, initializing data");
+            User user1 = new User();
+            Address address1 = new Address();
+            address1.setAddressTitle("EV");
+            address1.setAddressDescription("ISTANBUL TURKİYE");
+
+            user1.setName("Cem");
+            user1.setAge(30);
+            user1.setEmail("cem30@gmail.com");
+            user1.setAddress(address1);
+
+            userRepository.save(user1);
+
+            User user2 = new User();
+            Address address2 = new Address();
+            address2.setAddressTitle("EV");
+            address2.setAddressDescription("KARAMAN TURKİYE");
+
+            user2.setName("Cem");
+            user2.setAge(25);
+            user2.setEmail("cem25@gmail.com");
+            user2.setAddress(address2);
+
+            userRepository.save(user2);
+
+            User user3 = new User();
+            Address address3 = new Address();
+            address3.setAddressTitle("EV");
+            address3.setAddressDescription("ANKARA TURKİYE");
+
+            user3.setName("Cem");
+            user3.setAge(28);
+            user3.setEmail("cem28@gmail.com");
+            user3.setAddress(address3);
+
+            userRepository.save(user3);
+
+            User user4 = new User();
+            Address address4 = new Address();
+            address4.setAddressTitle("EV");
+            address4.setAddressDescription("ISTANBUL TURKİYE");
+
+            user4.setName("Cem");
+            user4.setAge(27);
+            user4.setEmail("cem27@gmail.com");
+            user4.setAddress(address4);
+
+            userRepository.save(user4);
+
+            User user5 = new User();
+            Address address5 = new Address();
+            address5.setAddressTitle("EV");
+            address5.setAddressDescription("MANISA TURKİYE");
+
+            user5.setName("Semih");
+            user5.setAge(24);
+            user5.setEmail("semih24@gmail.com");
+            user5.setAddress(address5);
+
+            userRepository.save(user5);
+
+            logger.info("Data initialized");
+        }
+
+    }
 }
