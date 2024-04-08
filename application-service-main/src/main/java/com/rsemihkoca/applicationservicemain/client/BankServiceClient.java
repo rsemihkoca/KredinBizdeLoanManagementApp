1f7db2c27d68b47d939f328a994920aca0ad6c9e
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-09 00:24:10 +0300

    application service init:application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/client/BankServiceClient.java

--- application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/client/BankServiceClient.java
+++ application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/client/BankServiceClient.java
@@ -1,7 +1,8 @@
 package com.rsemihkoca.applicationservicemain.client;
 
-import com.patika.kredinbizdeservice.client.dto.request.BankApplicationRequest;
-import com.patika.kredinbizdeservice.client.dto.response.ApplicationResponse;
+
+import com.rsemihkoca.applicationservicemain.dto.request.BankApplicationRequest;
+import com.rsemihkoca.applicationservicemain.dto.response.ApplicationResponse;
 
 public interface BankServiceClient {
     ApplicationResponse createApplication(BankApplicationRequest request);
