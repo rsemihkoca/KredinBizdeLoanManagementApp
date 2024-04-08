d310800b41bf1bd0b32fd308417e579ca8acdb8c
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-09 00:24:10 +0300

    application service init:application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/client/akbank/AkbankServiceClient.java

--- application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/client/akbank/AkbankServiceClient.java
+++ application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/client/akbank/AkbankServiceClient.java
@@ -1,8 +1,8 @@
 package com.rsemihkoca.applicationservicemain.client.akbank;
 
-import com.patika.kredinbizdeservice.client.BankServiceClient;
-import com.patika.kredinbizdeservice.client.dto.request.BankApplicationRequest;
-import com.patika.kredinbizdeservice.client.dto.response.ApplicationResponse;
+import com.rsemihkoca.applicationservicemain.client.BankServiceClient;
+import com.rsemihkoca.applicationservicemain.dto.request.BankApplicationRequest;
+import com.rsemihkoca.applicationservicemain.dto.response.ApplicationResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
