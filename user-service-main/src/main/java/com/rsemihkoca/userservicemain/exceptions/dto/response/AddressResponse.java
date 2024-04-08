196cac80f8bc98ca1db31991cf0111e5678c68d4
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-08 21:33:07 +0300

    userservice finished:user-service-main/src/main/java/com/rsemihkoca/userservicemain/exceptions/dto/response/AddressResponse.java

--- user-service-main/src/main/java/com/rsemihkoca/userservicemain/exceptions/dto/response/AddressResponse.java
+++ user-service-main/src/main/java/com/rsemihkoca/userservicemain/exceptions/dto/response/AddressResponse.java
@@ -3,7 +3,7 @@ package com.rsemihkoca.userservicemain.exceptions.dto.response;
 import lombok.Data;
 
 @Data
-public class AddressResponse {
+public class AddressResponse implements java.io.Serializable{
 
     private String addressTitle;
 
