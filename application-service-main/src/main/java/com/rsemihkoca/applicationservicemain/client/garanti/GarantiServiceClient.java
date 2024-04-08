3e5cad95b89365248aca89ea89892a9460dda5ee
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-09 00:24:10 +0300

    application service init:application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/client/garanti/GarantiServiceClient.java

--- application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/client/garanti/GarantiServiceClient.java
+++ application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/client/garanti/GarantiServiceClient.java
@@ -1,11 +1,12 @@
 package com.rsemihkoca.applicationservicemain.client.garanti;
 
-import com.patika.kredinbizdeservice.client.BankServiceClient;
-import com.patika.kredinbizdeservice.client.dto.request.BankApplicationRequest;
-import com.patika.kredinbizdeservice.client.dto.response.ApplicationResponse;
+import com.rsemihkoca.applicationservicemain.client.BankServiceClient;
+import com.rsemihkoca.applicationservicemain.dto.request.BankApplicationRequest;
+import com.rsemihkoca.applicationservicemain.dto.response.ApplicationResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
 
 @FeignClient(value = "garanti-service", url = "localhost:5555")
 public interface GarantiServiceClient extends BankServiceClient {
