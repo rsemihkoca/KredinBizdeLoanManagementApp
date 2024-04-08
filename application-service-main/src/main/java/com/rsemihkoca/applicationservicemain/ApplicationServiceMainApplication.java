495db1be45f36902f1e31dd5c34dd3db764f59d5
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-09 00:24:10 +0300

    application service init:application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/ApplicationServiceMainApplication.java

--- application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/ApplicationServiceMainApplication.java
+++ application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/ApplicationServiceMainApplication.java
@@ -2,8 +2,13 @@ package com.rsemihkoca.applicationservicemain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+//@ServletComponentScan
+@EnableDiscoveryClient
+@EnableCaching
 public class ApplicationServiceMainApplication {
 
 	public static void main(String[] args) {
