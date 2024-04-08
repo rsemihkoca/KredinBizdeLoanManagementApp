2664476868963f46b19e16f67c1986b0740dacc2
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-08 18:27:59 +0300

    user service ok:user-service-main/src/main/java/com/rsemihkoca/userservicemain/exceptions/dto/request/CreateUserRequest.java

--- user-service-main/src/main/java/com/rsemihkoca/userservicemain/exceptions/dto/request/CreateUserRequest.java
+++ user-service-main/src/main/java/com/rsemihkoca/userservicemain/exceptions/dto/request/CreateUserRequest.java
@@ -1,2 +1,19 @@
-package com.rsemihkoca.userservicemain.exceptions.dto.request;public class CreateUserRequest {
+package com.rsemihkoca.userservicemain.exceptions.dto.request;
+
+import com.rsemihkoca.userservicemain.model.Address;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class CreateUserRequest {
+
+    private String name;
+
+    private Integer age;
+
+    private String email;
+
+    private CreateAddressRequest address;
+
 }
