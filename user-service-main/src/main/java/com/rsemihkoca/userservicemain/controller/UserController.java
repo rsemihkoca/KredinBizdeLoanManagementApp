8af49c08689d607c618f6c84e4a096ebc6a64aa9
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-08 18:27:59 +0300

    user service ok:user-service-main/src/main/java/com/rsemihkoca/userservicemain/controller/UserController.java

--- user-service-main/src/main/java/com/rsemihkoca/userservicemain/controller/UserController.java
+++ user-service-main/src/main/java/com/rsemihkoca/userservicemain/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.rsemihkoca.userservicemain.controller;
 
 
+import com.rsemihkoca.userservicemain.exceptions.dto.request.CreateUserRequest;
+import com.rsemihkoca.userservicemain.exceptions.dto.response.UserResponse;
 import com.rsemihkoca.userservicemain.model.User;
 import com.rsemihkoca.userservicemain.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -17,22 +19,22 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public User createUser(@RequestBody User request) {
-        return userService.createUser(request);
+    public ResponseEntity<UserResponse> createUser(@RequestBody CreateUserRequest request) {
+        return ResponseEntity.ok(userService.createUser(request));
     }
 
     @GetMapping("/")
-    public ResponseEntity<List<User>> getAll() {
+    public ResponseEntity<List<UserResponse>> getAll() {
         return ResponseEntity.ok(userService.getAll());
     }
 
     @GetMapping("/{userId}")
-    public ResponseEntity<User> getById(@PathVariable("userId") String userId) {
+    public ResponseEntity<UserResponse> getById(@PathVariable("userId") Long userId) {
         return ResponseEntity.ok(userService.getById(userId));
     }
 
     @GetMapping("/errorTest")
-    public ResponseEntity<List<User>> testKafka() {
+    public ResponseEntity<List<UserResponse>> testKafka() {
         throw new RuntimeException("test exception");
     }
 
