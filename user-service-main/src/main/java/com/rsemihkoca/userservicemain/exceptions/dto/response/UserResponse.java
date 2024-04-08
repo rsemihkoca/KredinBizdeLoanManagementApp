e830c89f711749aced36f98a4a87eb7e5e983769
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-08 18:27:59 +0300

    user service ok:user-service-main/src/main/java/com/rsemihkoca/userservicemain/exceptions/dto/response/UserResponse.java

--- user-service-main/src/main/java/com/rsemihkoca/userservicemain/exceptions/dto/response/UserResponse.java
+++ user-service-main/src/main/java/com/rsemihkoca/userservicemain/exceptions/dto/response/UserResponse.java
@@ -1,2 +1,15 @@
-package com.rsemihkoca.userservicemain.exceptions.dto.request;public class UserResponse {
+package com.rsemihkoca.userservicemain.exceptions.dto.response;
+
+import lombok.Data;
+
+@Data
+public class UserResponse {
+
+    private String name;
+
+    private Integer age;
+
+    private String email;
+
+    private AddressResponse address;
 }
