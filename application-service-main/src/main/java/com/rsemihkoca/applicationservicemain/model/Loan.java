70a0bbe75bbf8be391a3f41324f2aa918d2728bd
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-09 16:57:14 +0300

    akbank and garanti client db connection:application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/model/Loan.java

--- application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/model/Loan.java
+++ application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/model/Loan.java
@@ -1,67 +1,36 @@
 package com.rsemihkoca.applicationservicemain.model;
 
+import com.rsemihkoca.applicationservicemain.enums.LoanType;
+import jakarta.persistence.*;
+import lombok.Data;
+import com.rsemihkoca.applicationservicemain.model.Constants.*;
 import java.math.BigDecimal;
 
-public abstract class Loan implements Product {
+@Data
+@Entity
+@Table(name = loanTable.TABLE_NAME)
+public class Loan implements java.io.Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = loanTable.LOAN_ID)
+    private Long loanId;
+
+    @Column(name = loanTable.AMOUNT, nullable = false)
     private BigDecimal amount;
-    private Integer installment;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = loanTable.LOAN_TYPE)
+    private LoanType loanType;
+
+    @OneToOne()
+    @JoinColumn(name = applicationTable.APPLICATION_ID, unique = true)
+    private Application application;
+
+    @OneToOne()
+    @JoinColumn(name = bankTable.BANK_ID, unique = true)
     private Bank bank;
-    private Double interestRate;
-    // private Campaign campaign; // kampanyalı kredileri üstte çıkart
 
-    //sponsorlu kampanyaları üstte çıkart
-
-    public Loan() {
-    }
-
-    public Loan(BigDecimal amount, Integer installment, Double interestRate) {
-        this.amount = amount;
-        this.installment = installment;
-        this.interestRate = interestRate;
-    }
-
-    abstract void calculate(BigDecimal amount, int installment);
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public Integer getInstallment() {
-        return installment;
-    }
-
-    public void setInstallment(Integer installment) {
-        this.installment = installment;
-    }
-
-    public Bank getBank() {
-        return bank;
-    }
-
-    public void setBank(Bank bank) {
-        this.bank = bank;
-    }
-
-    public Double getInterestRate() {
-        return interestRate;
-    }
-
-    public void setInterestRate(Double interestRate) {
-        this.interestRate = interestRate;
-    }
-
-    @Override
-    public String toString() {
-        return "Loan{" +
-                "amount=" + amount +
-                ", installment=" + installment +
-                ", bank=" + bank +
-                ", interestRate=" + interestRate +
-                '}';
-    }
 }
+
+
