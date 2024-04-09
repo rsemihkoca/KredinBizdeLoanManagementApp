50d14e6ed71c11c2be72a4a5ac0b8e464cf83a8d
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-09 22:50:13 +0300

    application servis error fixes cont.:application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/controller/ApplicationController.java

--- application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/controller/ApplicationController.java
+++ application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/controller/ApplicationController.java
@@ -1,38 +1,43 @@
-//package com.rsemihkoca.applicationservicemain.controller;
-//
-//import com.rsemihkoca.applicationservicemain.dto.request.ApplicationRequest;
-//import com.rsemihkoca.applicationservicemain.model.Application;
-//import com.rsemihkoca.applicationservicemain.service.ApplicationService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("api/v1/application")
-//@RequiredArgsConstructor
-//public class ApplicationController {
-//
-//    private final ApplicationService applicationService;
-//
-//    @PostMapping
-//    @ResponseStatus(HttpStatus.CREATED)
-//    public ResponseEntity<Application> createApplication(@RequestBody ApplicationRequest request) {
-//        return ResponseEntity.ok().body(applicationService.createApplication(request));
-//    }
-//
-//    // get application of user by email
-//
-//    @GetMapping("/{email}")
-//    public ResponseEntity<List<Application>> getAll(@PathVariable String email) {
-//        return ResponseEntity.ok().body(applicationService.getAll(email));
-//    }
-//
-//    @GetMapping
-//    public ResponseEntity<List<Application>> testKafka() {
-//        throw new RuntimeException("test exception");
-//    }
-//
-//}
+package com.rsemihkoca.applicationservicemain.controller;
+
+import com.rsemihkoca.applicationservicemain.dto.request.ApplicationRequest;
+import com.rsemihkoca.applicationservicemain.dto.response.UserResponse;
+import com.rsemihkoca.applicationservicemain.model.Application;
+import com.rsemihkoca.applicationservicemain.service.ApplicationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/application")
+@RequiredArgsConstructor
+public class ApplicationController {
+
+    private final ApplicationService applicationService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Application> createApplication(@RequestBody ApplicationRequest request) {
+        return ResponseEntity.ok().body(applicationService.createApplication(request));
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<List<Application>> getByEmail(@PathVariable String email) {
+        return ResponseEntity.ok().body(applicationService.getByEmail(email));
+    }
+
+
+    @GetMapping("/")
+    public ResponseEntity<List<Application>> getAll() {
+        return ResponseEntity.ok().body(applicationService.getAll());
+    }
+
+    @GetMapping("/errorTest")
+    public ResponseEntity<List<UserResponse>> testKafka() {
+        throw new RuntimeException("test exception");
+    }
+
+}
