905299f11990ee73affe478bce15b81b62cfc4d6
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-09 22:50:13 +0300

    application servis error fixes cont.:application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/client/userservice/UserServiceClient.java

--- application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/client/userservice/UserServiceClient.java
+++ application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/client/userservice/UserServiceClient.java
@@ -1,15 +1,15 @@
 package com.rsemihkoca.applicationservicemain.client.userservice;
 
+//import com.rsemihkoca.applicationservicemain.client.IUserServiceClient;
 import com.rsemihkoca.applicationservicemain.dto.response.UserResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "user-service-main", url = "localhost:8081")
+@FeignClient(value = "user-service", url = "http://localhost:8081/api/v1/user/email")
 public interface UserServiceClient {
 
     @GetMapping("/{email}")
     ResponseEntity<UserResponse> getByEmail(@PathVariable String email);
-
 }
