644a22979c264d6cb4055c71b697c09163095a30
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-09 22:50:13 +0300

    application servis error fixes cont.:application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/client/IUserServiceClient.java

--- application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/client/IUserServiceClient.java
+++ application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/client/IUserServiceClient.java
@@ -1,4 +1,9 @@
-package com.rsemihkoca.applicationservicemain.client;
-
-public interface IUserServiceClient {
-}
+//package com.rsemihkoca.applicationservicemain.client;
+//
+//import com.rsemihkoca.applicationservicemain.dto.response.UserResponse;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.PathVariable;
+//
+//public interface IUserServiceClient {
+//    ResponseEntity<UserResponse> getByEmail(String email);
+//}
