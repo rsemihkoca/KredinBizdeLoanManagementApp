7d8fbb73f510b19d14780725f9ff15751807080f
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-09 16:57:14 +0300

    akbank and garanti client db connection:application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/converter/ApplicationConverter.java

--- application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/converter/ApplicationConverter.java
+++ application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/converter/ApplicationConverter.java
@@ -1,21 +1,20 @@
-package com.rsemihkoca.applicationservicemain.converter;
-
-import com.patika.kredinbizdeservice.dto.request.ApplicationRequest;
-import com.patika.kredinbizdeservice.enums.ApplicationStatus;
-import com.patika.kredinbizdeservice.model.Application;
-import com.patika.kredinbizdeservice.model.User;
-import org.springframework.stereotype.Component;
-
-import java.time.LocalDateTime;
-
-@Component
-public class ApplicationConverter {
-
-    public Application toApplication(ApplicationRequest request, User user) {
-        Application application = new Application();
-        application.setApplicationStatus(ApplicationStatus.IN_PROGRESS);
-        application.setUser(user);
-        application.setLocalDateTime(LocalDateTime.now());
-        return application;
-    }
-}
+//package com.rsemihkoca.applicationservicemain.converter;
+//
+//
+//import com.rsemihkoca.applicationservicemain.dto.request.ApplicationRequest;
+//import com.rsemihkoca.applicationservicemain.model.Application;
+//import org.springframework.stereotype.Component;
+//
+//import java.time.LocalDateTime;
+//
+//@Component
+//public class ApplicationConverter {
+//
+//    public Application toApplication(ApplicationRequest request, User user) {
+//        Application application = new Application();
+//        application.setApplicationStatus(ApplicationStatus.IN_PROGRESS);
+//        application.setUser(user);
+//        application.setLocalDateTime(LocalDateTime.now());
+//        return application;
+//    }
+//}
