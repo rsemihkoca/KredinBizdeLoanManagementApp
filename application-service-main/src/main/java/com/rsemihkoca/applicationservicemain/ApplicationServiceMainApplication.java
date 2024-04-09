560af233b8a3a5286e6a029fd059f044477df7ea
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-09 22:50:13 +0300

    application servis error fixes cont.:application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/ApplicationServiceMainApplication.java

--- application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/ApplicationServiceMainApplication.java
+++ application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/ApplicationServiceMainApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-//@ServletComponentScan
 @EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.rsemihkoca.applicationservicemain.client")
 @EnableCaching
 public class ApplicationServiceMainApplication {
 
