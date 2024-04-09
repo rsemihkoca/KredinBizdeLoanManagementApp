d527e172c19d9c7e4fa353d6d651131d7bba1ff2
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-09 16:57:14 +0300

    akbank and garanti client db connection:application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/model/Constants.java

--- application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/model/Constants.java
+++ application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/model/Constants.java
@@ -9,19 +9,35 @@ import org.springframework.stereotype.Component;
 public final class Constants {
 
     @NoArgsConstructor(access = AccessLevel.PRIVATE)
-    public static class userTable {
-        public static final String TABLE_NAME = "USER";
-        public static final String USER_ID = "USER_ID";
-        public static final String NAME = "NAME";
-        public static final String EMAIL = "EMAIL";
-        public static final String AGE = "AGE";
+    public static class applicationTable {
+        public static final String TABLE_NAME = "APPLICATION";
+        public static final String APPLICATION_ID = "APPLICATION_ID";
+        public static final String IS_ACTIVE = "IS_ACTIVE";
+        public static final String APPLICATION_STATUS = "APPLICATION_STATUS";
     }
 
     @NoArgsConstructor(access = AccessLevel.PRIVATE)
-    public static class addressTable {
-        public static final String TABLE_NAME = "ADDRESS";
-        public static final String ADDRESS_ID = "ADDRESS_ID";
-        public static final String ADDRESS_TITLE = "ADDRESS_TITLE";
-        public static final String ADDRESS_DESCRIPTION = "ADDRESS_DESCRIPTION";
+    public static class bankTable {
+        public static final String TABLE_NAME = "BANK";
+        public static final String BANK_ID = "BANK_ID";
+        public static final String NAME = "NAME";
     }
+
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class loanTable {
+        public static final String TABLE_NAME = "LOAN";
+        public static final String LOAN_ID = "LOAN_ID";
+        public static final String AMOUNT = "AMOUNT";
+        public static final String LOAN_TYPE = "LOAN_TYPE";
+
+    }
+
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class userTable {
+        public static final String TABLE_NAME = "USER";
+        public static final String USER_ID = "USER_ID";
+        public static final String EMAIL = "EMAIL";
+    }
+
+
 }
