b1f9833e47484f8d2bd81a6490c69e3dc960cc83
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-09 16:57:14 +0300

    akbank and garanti client db connection:application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/client/akbank/AkbankClient.java

--- application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/client/akbank/AkbankClient.java
+++ application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/client/akbank/AkbankClient.java
@@ -3,6 +3,8 @@ package com.rsemihkoca.applicationservicemain.client.akbank;
 import com.rsemihkoca.applicationservicemain.client.BankServiceClient;
 import com.rsemihkoca.applicationservicemain.dto.request.BankApplicationRequest;
 import com.rsemihkoca.applicationservicemain.dto.response.ApplicationResponse;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,9 +12,14 @@ public class AkbankClient implements BankServiceClient {
 
     private final AkbankServiceClient feignClient;
 
+    public AkbankClient() {
+        this.feignClient = null;
+    }
+
     public AkbankClient(AkbankServiceClient feignClient) {
         this.feignClient = feignClient;
     }
+
     @Override
     public ApplicationResponse createApplication(BankApplicationRequest request) {
         if (feignClient == null) {
