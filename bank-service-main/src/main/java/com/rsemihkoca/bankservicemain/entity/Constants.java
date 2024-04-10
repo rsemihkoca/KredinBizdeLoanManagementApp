b60cc90ec410472a5ac78e6e45b633f3a7be1e47
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-11 01:22:12 +0300

    application service and notification service refactoring:bank-service-main/src/main/java/com/rsemihkoca/bankservicemain/entity/Constants.java

--- bank-service-main/src/main/java/com/rsemihkoca/bankservicemain/entity/Constants.java
+++ bank-service-main/src/main/java/com/rsemihkoca/bankservicemain/entity/Constants.java
@@ -1,2 +1,12 @@
-package com.rsemihkoca.bankservicemain.entity;public class Constants {
+package com.rsemihkoca.bankservicemain.entity;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class Constants {
+
+    public static final String LOAN_CACHE = "LOAN_CACHE";
 }
