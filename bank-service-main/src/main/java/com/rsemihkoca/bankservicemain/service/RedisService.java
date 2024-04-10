976b4d9fd904486b1f4a46b9ce74bf5f685ef26c
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-11 01:22:12 +0300

    application service and notification service refactoring:bank-service-main/src/main/java/com/rsemihkoca/bankservicemain/service/RedisService.java

--- bank-service-main/src/main/java/com/rsemihkoca/bankservicemain/service/RedisService.java
+++ bank-service-main/src/main/java/com/rsemihkoca/bankservicemain/service/RedisService.java
@@ -1,2 +1,25 @@
-package com.rsemihkoca.bankservicemain.service;public class RedisService {
+package com.rsemihkoca.bankservicemain.service;
+
+import com.rsemihkoca.bankservicemain.dto.response.MergedLoanResponse;
+import com.rsemihkoca.bankservicemain.entity.Loan;
+import com.rsemihkoca.bankservicemain.repository.RedisRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class RedisService {
+
+    private RedisTemplate<String, List<MergedLoanResponse>> redisTemplate;
+
+    public List<MergedLoanResponse> getAll(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public void save(String key, List<MergedLoanResponse> mergedLoanResponses) {
+        redisTemplate.opsForValue().set(key, mergedLoanResponses);
+    }
 }
