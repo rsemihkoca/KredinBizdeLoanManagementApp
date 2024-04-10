47e21b0dba3456e7201b8e6459ebef9ea9e14808
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-10 04:22:43 +0300

    akbank service:akbank-service-main/src/main/java/com/rsemihkoca/akbankservice/enums/LoanType.java

--- akbank-service-main/src/main/java/com/rsemihkoca/akbankservice/enums/LoanType.java
+++ akbank-service-main/src/main/java/com/rsemihkoca/akbankservice/enums/LoanType.java
@@ -1,4 +1,12 @@
 package com.rsemihkoca.akbankservice.enums;
 
 public enum LoanType {
+
+    IHTIYAC_KREDISI,
+    KONUT_KREDISI,
+    ARAC_KREDISI,
+    TATIL_KREDISI,
+    ESNAF_KREDISI,
+    OGRENCI_KREDISI,
+    SAGLIK_KREDISI,
 }
