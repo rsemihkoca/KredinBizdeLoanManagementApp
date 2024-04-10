681c702fabcf7713726c79d75c27c1ac268031a7
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-11 01:22:12 +0300

    application service and notification service refactoring:bank-service-main/src/main/java/com/rsemihkoca/bankservicemain/BankServiceMainApplication.java

--- bank-service-main/src/main/java/com/rsemihkoca/bankservicemain/BankServiceMainApplication.java
+++ bank-service-main/src/main/java/com/rsemihkoca/bankservicemain/BankServiceMainApplication.java
@@ -2,8 +2,15 @@ package com.rsemihkoca.bankservicemain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableScheduling
 public class BankServiceMainApplication {
 
     public static void main(String[] args) {
