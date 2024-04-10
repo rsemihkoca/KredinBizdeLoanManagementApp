ffefce8aab3b142851418388fba6200210ebbe2d
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-10 04:22:43 +0300

    akbank service:application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/client/ClientFactory.java

--- application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/client/ClientFactory.java
+++ application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/client/ClientFactory.java
@@ -2,8 +2,10 @@ package com.rsemihkoca.applicationservicemain.client;
 
 import com.rsemihkoca.applicationservicemain.client.akbank.AkbankClient;
 import com.rsemihkoca.applicationservicemain.client.garanti.GarantiClient;
-import com.rsemihkoca.applicationservicemain.client.userservice.UserClient;
-import com.rsemihkoca.applicationservicemain.client.userservice.UserServiceClient;
+//import com.rsemihkoca.applicationservicemain.client.userservice.UserClient;
+//import com.rsemihkoca.applicationservicemain.client.userservice.UserServiceClient;
+import com.rsemihkoca.applicationservicemain.dto.request.ApplicationRequest;
+import com.rsemihkoca.applicationservicemain.dto.request.BankApplicationRequest;
 import com.rsemihkoca.applicationservicemain.enums.BankType;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -13,13 +15,19 @@ import org.springframework.stereotype.Component;
 public class ClientFactory {
     private final GarantiClient garantiClient;
     private final AkbankClient akbankClient;
-    private final UserClient userClient;
 
-    public ClientFactory(GarantiClient garantiClient, AkbankClient akbankClient, UserClient userClient) {
+    public ClientFactory(GarantiClient garantiClient, AkbankClient akbankClient) {
         this.garantiClient = garantiClient;
         this.akbankClient = akbankClient;
-        this.userClient = userClient;
     }
+
+//    private void sendApplication(ApplicationRequest request, User user) {
+//        BankType bankType = BankType.valueOf(request.getBankName().toUpperCase());
+//        BankServiceClient bankServiceClient = clientFactory.createBankClient(bankType);
+//        BankApplicationRequest bankApplicationRequest = new BankApplicationRequest();
+//        bankApplicationRequest.setUserId(user.getUserId());
+//        bankServiceClient.createApplication(bankApplicationRequest);
+//    }
 
     public BankServiceClient createBankClient(BankType bankName) {
         return switch (bankName) {
@@ -28,7 +36,4 @@ public class ClientFactory {
         };
     }
 
-    public UserClient createUserServiceClient() {
-        return userClient;
-    }
 }
