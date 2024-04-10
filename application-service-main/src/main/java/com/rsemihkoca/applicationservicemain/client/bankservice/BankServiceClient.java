92f62981c784c09661b618127bd643349862698f
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-11 01:22:12 +0300

    application service and notification service refactoring:application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/client/bankservice/BankServiceClient.java

--- application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/client/bankservice/BankServiceClient.java
+++ application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/client/bankservice/BankServiceClient.java
@@ -1,15 +1,17 @@
 package com.rsemihkoca.applicationservicemain.client.bankservice;
 
-//import com.rsemihkoca.applicationservicemain.client.IUserServiceClient;
-import com.rsemihkoca.applicationservicemain.dto.response.UserResponse;
+import com.rsemihkoca.applicationservicemain.dto.response.GenericResponse;
+import com.rsemihkoca.applicationservicemain.dto.response.MergedLoanResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "user-service", url = "http://localhost:8081/api/v1/user/email")
+import java.util.List;
+
+
+@FeignClient(name = "bank-service")
 public interface BankServiceClient {
 
-    @GetMapping("/{email}")
-    ResponseEntity<UserResponse> getByEmail(@PathVariable String email);
+    @GetMapping("/api/v1/loan")
+    ResponseEntity<GenericResponse<List<MergedLoanResponse>>> getAll();
 }
