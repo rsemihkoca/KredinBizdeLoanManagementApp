1e68c54ecbb95a0196d47c6b365a01691f6432cd
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-11 01:22:12 +0300

    application service and notification service refactoring:bank-service-main/src/main/java/com/rsemihkoca/bankservicemain/controller/LoanController.java

--- bank-service-main/src/main/java/com/rsemihkoca/bankservicemain/controller/LoanController.java
+++ bank-service-main/src/main/java/com/rsemihkoca/bankservicemain/controller/LoanController.java
@@ -1,49 +1,23 @@
-package com.rsemihkoca.akbankservice.controller;
+package com.rsemihkoca.bankservicemain.controller;
 
-import com.rsemihkoca.akbankservice.dto.response.GenericResponse;
-import com.rsemihkoca.akbankservice.dto.response.LoanResponse;
+import com.rsemihkoca.bankservicemain.dto.response.GenericResponse;
+import com.rsemihkoca.bankservicemain.dto.response.MergedLoanResponse;
+import com.rsemihkoca.bankservicemain.service.LoanService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.rsemihkoca.akbankservice.dto.request.LoanRequest;
-import com.rsemihkoca.akbankservice.service.LoanService;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("akbank/v1/api/")
+@RequestMapping("/api/v1/")
 @RequiredArgsConstructor
 public class LoanController {
 
     private final LoanService loanService;
 
-    @PostMapping("loan")
-    public ResponseEntity<GenericResponse<LoanResponse>> createLoan(@RequestBody LoanRequest request) {
-        return ResponseEntity.ok(GenericResponse.success(loanService.createLoan(request)));
-    }
-
     @GetMapping("loan")
-    public ResponseEntity<GenericResponse<List<LoanResponse>>> getAll() {
+    public ResponseEntity<GenericResponse<List<MergedLoanResponse>>> getAll() {
         return ResponseEntity.ok(GenericResponse.success(loanService.getAll()));
-    }
-
-    @GetMapping("loan/{loanId}")
-    public ResponseEntity<GenericResponse<LoanResponse>> getById(@PathVariable("loanId") Long loanId) {
-        return ResponseEntity.ok(GenericResponse.success(loanService.getById(loanId)));
-    }
-
-    @DeleteMapping("loan/{loanId}")
-    public ResponseEntity<GenericResponse<LoanResponse>> deleteById(@PathVariable("loanId") Long loanId) {
-        return ResponseEntity.ok(GenericResponse.success(loanService.deleteById(loanId)));
-    }
-
-    @PutMapping("loan/{loanId}")
-    public ResponseEntity<GenericResponse<LoanResponse>> updateLoan(@PathVariable("loanId") Long loanId, @RequestBody LoanRequest request) {
-        return ResponseEntity.ok(GenericResponse.success(loanService.updateLoan(loanId, request)));
-    }
-
-    @PostMapping("errorTest")
-    public ResponseEntity<GenericResponse<LoanResponse>> testKafka() {
-        throw new RuntimeException("test exception");
     }
 }
