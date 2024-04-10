01ddf3c15987371861a6ddc218b21f71c8f95407
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-10 04:22:43 +0300

    akbank service:akbank-service-main/src/main/java/com/rsemihkoca/akbankservice/repository/LoanRepository.java

--- akbank-service-main/src/main/java/com/rsemihkoca/akbankservice/repository/LoanRepository.java
+++ akbank-service-main/src/main/java/com/rsemihkoca/akbankservice/repository/LoanRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
-
+@Repository
 public interface LoanRepository extends JpaRepository<Loan, Long> {
 
+    Loan getLoanById(Long loanId);
 }
 
