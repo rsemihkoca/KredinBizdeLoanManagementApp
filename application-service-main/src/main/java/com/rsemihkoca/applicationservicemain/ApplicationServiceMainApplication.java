60c5402ad6212fb15413cf6300dd4528fbe36593
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-11 01:22:12 +0300

    application service and notification service refactoring:application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/ApplicationServiceMainApplication.java

--- application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/ApplicationServiceMainApplication.java
+++ application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/ApplicationServiceMainApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients(basePackages = "com.rsemihkoca.applicationservicemain.client")
+@EnableFeignClients
 @EnableCaching
 public class ApplicationServiceMainApplication {
 
