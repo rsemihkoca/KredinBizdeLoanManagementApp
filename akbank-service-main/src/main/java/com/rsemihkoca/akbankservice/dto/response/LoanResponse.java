ab55914d79024704aaab87fc028e2125172af9cb
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-10 04:22:43 +0300

    akbank service:akbank-service-main/src/main/java/com/rsemihkoca/akbankservice/dto/response/LoanResponse.java

--- akbank-service-main/src/main/java/com/rsemihkoca/akbankservice/dto/response/LoanResponse.java
+++ akbank-service-main/src/main/java/com/rsemihkoca/akbankservice/dto/response/LoanResponse.java
@@ -1,32 +1,27 @@
 package com.rsemihkoca.akbankservice.dto.response;
 
-import com.rsemihkoca.akbankservice.entity.Constants;
-import com.rsemihkoca.akbankservice.enums.BankApplicationStatus;
-import jakarta.persistence.*;
 import lombok.*;
 
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@Data
 @ToString
 @Builder
-public class LoanResponse implements Serializable{
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoanResponse implements Serializable {
 
-        private LocalDate CreateDate;
 
-        private LocalDate UpdateDate;
+    private String type;
 
-        private String Type;
+    private Double amount;
 
-        private Double Amount;
+    private Double interestRate;
 
-        private Double InterestRate;
+    private Integer duration;
+    private String createDate;
 
-        private Integer Duration;
-
+    private String updateDate;
 }
