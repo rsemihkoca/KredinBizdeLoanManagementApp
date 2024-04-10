4b591d960417939887d163dc50933d4002c6f5a0
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-11 01:22:12 +0300

    application service and notification service refactoring:bank-service-main/src/main/java/com/rsemihkoca/bankservicemain/configuration/BeanConfiguration.java

--- bank-service-main/src/main/java/com/rsemihkoca/bankservicemain/configuration/BeanConfiguration.java
+++ bank-service-main/src/main/java/com/rsemihkoca/bankservicemain/configuration/BeanConfiguration.java
@@ -1,6 +1,5 @@
 package com.rsemihkoca.bankservicemain.configuration;
 
-import com.rsemihkoca.akbankservice.mapper.LoanRequestToLoanConverter;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.config.Configuration.AccessLevel;
 import org.modelmapper.convention.MatchingStrategies;
@@ -19,7 +18,7 @@ public class BeanConfiguration {
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper mapper = new ModelMapper();
-        mapper.addConverter(new LoanRequestToLoanConverter());
+//        mapper.addConverter(new LoanRequestToLoanConverter());
 
         mapper.getConfiguration()
                 .setFieldMatchingEnabled(true)
@@ -29,11 +28,4 @@ public class BeanConfiguration {
         return mapper;
     }
 
-    @Bean
-    public RedisCacheConfiguration cacheConfiguration() {
-        return RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofMinutes(5))
-                .disableCachingNullValues()
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
-    }
 }
