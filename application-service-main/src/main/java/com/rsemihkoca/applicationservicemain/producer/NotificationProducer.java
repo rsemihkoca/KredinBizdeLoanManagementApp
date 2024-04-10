e2282642ef87d761f004c6669669358791bbaae9
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-10 04:22:43 +0300

    akbank service:application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/producer/NotificationProducer.java

--- application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/producer/NotificationProducer.java
+++ application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/producer/NotificationProducer.java
@@ -1,25 +1,25 @@
-package com.rsemihkoca.applicationservicemain.producer;
-
-
-import com.rsemihkoca.applicationservicemain.configuration.RabbitMQConfiguration;
-import com.rsemihkoca.applicationservicemain.producer.dto.NotificationDTO;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.stereotype.Component;
-
-@Component
-@RequiredArgsConstructor
-@Slf4j
-public class NotificationProducer {
-
-
-    private final RabbitMQConfiguration rabbitMQConfiguration;
-    private final AmqpTemplate amqpTemplate;
-
-    public void sendNotification(NotificationDTO notificationDTO) {
-        log.info("notification sent: {}", notificationDTO);
-        amqpTemplate.convertSendAndReceive(rabbitMQConfiguration.getExchange(), rabbitMQConfiguration.getRoutingkey(), notificationDTO);
-    }
-
-}
+//package com.rsemihkoca.applicationservicemain.producer;
+//
+//
+//import com.rsemihkoca.applicationservicemain.configuration.RabbitMQConfiguration;
+//import com.rsemihkoca.applicationservicemain.producer.dto.NotificationDTO;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.amqp.core.AmqpTemplate;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@RequiredArgsConstructor
+//@Slf4j
+//public class NotificationProducer {
+//
+//
+//    private final RabbitMQConfiguration rabbitMQConfiguration;
+//    private final AmqpTemplate amqpTemplate;
+//
+//    public void sendNotification(NotificationDTO notificationDTO) {
+//        log.info("notification sent: {}", notificationDTO);
+//        amqpTemplate.convertSendAndReceive(rabbitMQConfiguration.getExchange(), rabbitMQConfiguration.getRoutingkey(), notificationDTO);
+//    }
+//
+//}
