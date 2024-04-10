3cf8e2af8fd6640ee5ac00ea3d8f83040e6fb1c4
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-11 01:22:12 +0300

    application service and notification service refactoring:application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/client/userservice/UserServiceClient.java

--- application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/client/userservice/UserServiceClient.java
+++ application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/client/userservice/UserServiceClient.java
@@ -7,9 +7,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "user-service", url = "http://localhost:8081/api/v1/user/email")
+//@FeignClient(value = "user-service", url = "http://localhost:8081/api/v1/user/email")
+//public interface UserServiceClient {
+//
+//    @GetMapping("/{email}")
+//    ResponseEntity<UserResponse> getByEmail(@PathVariable String email);
+//}
+
+@FeignClient(value = "user-service")
 public interface UserServiceClient {
 
-    @GetMapping("/{email}")
+    @GetMapping("/api/v1/user/email/{email}")
     ResponseEntity<UserResponse> getByEmail(@PathVariable String email);
 }
