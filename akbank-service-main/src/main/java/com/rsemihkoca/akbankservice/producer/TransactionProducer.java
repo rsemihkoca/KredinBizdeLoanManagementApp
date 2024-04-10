2a5f0bb14f439a388330f18a09f9a4b3d1dcd60e
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-10 04:22:43 +0300

    akbank service:akbank-service-main/src/main/java/com/rsemihkoca/akbankservice/producer/TransactionProducer.java

--- akbank-service-main/src/main/java/com/rsemihkoca/akbankservice/producer/TransactionProducer.java
+++ akbank-service-main/src/main/java/com/rsemihkoca/akbankservice/producer/TransactionProducer.java
@@ -1,8 +1,8 @@
-package com.rsemihkoca.applicationservicemain.producer;
+package com.rsemihkoca.akbankservice.producer;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.rsemihkoca.applicationservicemain.producer.dto.Transaction;
+import com.rsemihkoca.akbankservice.producer.dto.Transaction;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
