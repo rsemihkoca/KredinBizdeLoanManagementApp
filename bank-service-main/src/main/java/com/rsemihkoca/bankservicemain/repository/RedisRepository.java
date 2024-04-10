c08c0a845c411fdc4466281873d779e17d7e5c7a
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-11 01:22:12 +0300

    application service and notification service refactoring:bank-service-main/src/main/java/com/rsemihkoca/bankservicemain/repository/RedisRepository.java

--- bank-service-main/src/main/java/com/rsemihkoca/bankservicemain/repository/RedisRepository.java
+++ bank-service-main/src/main/java/com/rsemihkoca/bankservicemain/repository/RedisRepository.java
@@ -1,2 +1,9 @@
-package com.rsemihkoca.bankservicemain.repository;public class RedisRepository {
+package com.rsemihkoca.bankservicemain.repository;
+
+import com.rsemihkoca.bankservicemain.dto.response.MergedLoanResponse;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface RedisRepository extends CrudRepository<List<MergedLoanResponse>, String> {
 }
