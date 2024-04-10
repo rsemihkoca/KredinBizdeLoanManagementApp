3f5ae6daae544c53a0a6d63238dbacf35a69ae79
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-11 01:22:12 +0300

    application service and notification service refactoring:application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/enums/NotificationContent.java

--- application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/enums/NotificationContent.java
+++ application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/enums/NotificationContent.java
@@ -1,2 +1,9 @@
-package com.rsemihkoca.applicationservicemain.enums;public enum NotificationContent {
+package com.rsemihkoca.applicationservicemain.enums;
+
+public enum NotificationContent {
+
+    APPLICATION_CREATED,
+    APPLICATION_UPDATED,
+    APPLICATION_DELETED,
+//    APPLICATION_APPROVED,
 }
