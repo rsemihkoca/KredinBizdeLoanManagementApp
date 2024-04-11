08dedce22aaedd3ac23c835f7978002e350ea4b9
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-11 22:38:45 +0300

    fully working first time:application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/repository/ApplicationRepository.java

--- application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/repository/ApplicationRepository.java
+++ application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/repository/ApplicationRepository.java
@@ -2,17 +2,20 @@ package com.rsemihkoca.applicationservicemain.repository;
 
 import com.rsemihkoca.applicationservicemain.model.Application;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
-
 
 @Repository
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
+//public class ApplicationRepository {
+    @Query("SELECT a FROM Application a WHERE a.isActive = true AND a.userEmail = :email")
+    List<Application> findActiveApplicationsByUserEmail(@Param("email") String userEmail);
+//
+    @Query("SELECT a FROM Application a WHERE a.isActive = true")
+    List<Application> findAllActiveApplications();
 
-    List<Application> findByUserEmailAndIsActive(String userEmail, boolean isActive);
 
-
-    List<Application> findAllByIsActive(boolean isActive);
 }
