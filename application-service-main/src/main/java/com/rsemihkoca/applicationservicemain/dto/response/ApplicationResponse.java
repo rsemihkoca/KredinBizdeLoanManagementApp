b65cc87748fba4b29d311dfef24e8eab22c106ce
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-11 22:38:45 +0300

    fully working first time:application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/dto/response/ApplicationResponse.java

--- application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/dto/response/ApplicationResponse.java
+++ application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/dto/response/ApplicationResponse.java
@@ -1,15 +1,18 @@
 package com.rsemihkoca.applicationservicemain.dto.response;
 
-import com.rsemihkoca.applicationservicemain.enums.ApplicationStatus;
 import lombok.*;
-
-import java.time.LocalDateTime;
 
 @Builder
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ApplicationResponse implements java.io.Serializable{
 
     private String userEmail;
-    private LocalDateTime createDate;
-    private ApplicationStatus applicationStatus;
+    private String applicationDate;
+    private String applicationStatus;
+
 }
+
+
+
