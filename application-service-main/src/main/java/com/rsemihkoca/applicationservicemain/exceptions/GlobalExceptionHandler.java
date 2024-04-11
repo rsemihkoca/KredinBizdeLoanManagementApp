0011e519477f5fa34892756108c2e811782db8f4
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-12 01:04:01 +0300

    user inactivation addded:application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/exceptions/GlobalExceptionHandler.java

--- application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/exceptions/GlobalExceptionHandler.java
+++ application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,8 @@
 package com.rsemihkoca.applicationservicemain.exceptions;
 
 import com.rsemihkoca.applicationservicemain.dto.response.ExceptionResponse;
-import com.rsemihkoca.applicationservicemain.producer.TransactionProducer;
+import com.rsemihkoca.applicationservicemain.dto.response.GenericResponse;
+import com.rsemihkoca.applicationservicemain.producer.GenericKafkaProducer;
 import com.rsemihkoca.applicationservicemain.producer.dto.Transaction;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -15,20 +16,22 @@ import java.time.LocalDateTime;
 @Slf4j
 public class GlobalExceptionHandler {
 
-    private final TransactionProducer transactionProducer;
+    private final GenericKafkaProducer genericKafkaProducer;
 
-    public GlobalExceptionHandler(TransactionProducer transactionProducer) {
-        this.transactionProducer = transactionProducer;
+    public GlobalExceptionHandler(GenericKafkaProducer genericKafkaProducer) {
+        this.genericKafkaProducer = genericKafkaProducer;
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ExceptionResponse> handleAllException(Exception exception) {
-        log.error("exception occurred. {0}", exception.getCause());
-        transactionProducer.sendTransaction(prepareTransaction(exception));
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<GenericResponse<ExceptionResponse>> handleAllException(Exception exception) {
+        log.error("exception occurred. {0}", exception.getCause());
+        genericKafkaProducer.sendTransaction(prepareTransaction(exception));
+
+        GenericResponse<ExceptionResponse> response = GenericResponse.error(prepareExceptionResponse(exception));
         return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(prepareExceptionResponse(exception));
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(response);
     }
 
     private ExceptionResponse prepareExceptionResponse(Exception exception) {
