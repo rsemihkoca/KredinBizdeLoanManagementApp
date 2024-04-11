07c7b45fe73d890e25cca7593d026403464c72d3
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-11 22:38:45 +0300

    fully working first time:application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/mapper/ApplicationToApplicationResponse.java

--- application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/mapper/ApplicationToApplicationResponse.java
+++ application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/mapper/ApplicationToApplicationResponse.java
@@ -1,2 +1,20 @@
-package com.rsemihkoca.applicationservicemain.mapper;public class ApplicationToApplicationResponse {
+package com.rsemihkoca.applicationservicemain.mapper;
+
+import com.rsemihkoca.applicationservicemain.dto.response.ApplicationResponse;
+import com.rsemihkoca.applicationservicemain.model.Application;
+import org.modelmapper.Converter;
+import org.modelmapper.spi.MappingContext;
+
+public class ApplicationToApplicationResponse implements Converter<Application, ApplicationResponse> {
+
+
+    @Override
+    public ApplicationResponse convert(MappingContext<Application, ApplicationResponse> mappingContext) {
+        Application application = mappingContext.getSource();
+        return ApplicationResponse.builder()
+                .userEmail(application.getUserEmail())
+                .applicationStatus(application.getApplicationStatus().toString())
+                .applicationDate(application.getApplicationDate())
+                .build();
+    }
 }
