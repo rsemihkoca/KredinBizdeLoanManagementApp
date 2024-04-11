fbbdd89103fd2ee06fd449f5927afdd474def496
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-11 18:46:42 +0300

    feign client fix:notification-service-main/src/main/java/com/patika/notificationservice/NotificationServiceApplication.java

--- notification-service-main/src/main/java/com/patika/notificationservice/NotificationServiceApplication.java
+++ notification-service-main/src/main/java/com/patika/notificationservice/NotificationServiceApplication.java
@@ -2,8 +2,12 @@ package com.patika.notificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NotificationServiceApplication {
 
     public static void main(String[] args) {
