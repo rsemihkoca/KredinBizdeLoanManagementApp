4baecf68c5800c8ba03d783f17109dd14073c327
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-11 18:46:42 +0300

    feign client fix:user-service-main/src/main/java/com/rsemihkoca/userservicemain/controller/UserController.java

--- user-service-main/src/main/java/com/rsemihkoca/userservicemain/controller/UserController.java
+++ user-service-main/src/main/java/com/rsemihkoca/userservicemain/controller/UserController.java
@@ -2,7 +2,7 @@ package com.rsemihkoca.userservicemain.controller;
 
 
 import com.rsemihkoca.userservicemain.exceptions.dto.request.CreateUserRequest;
-import com.rsemihkoca.userservicemain.exceptions.dto.response.ApiResponse;
+import com.rsemihkoca.userservicemain.exceptions.dto.response.GenericResponse;
 import com.rsemihkoca.userservicemain.exceptions.dto.response.UserResponse;
 import com.rsemihkoca.userservicemain.model.User;
 import com.rsemihkoca.userservicemain.service.UserService;
@@ -20,27 +20,27 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public ResponseEntity<UserResponse> createUser(@RequestBody CreateUserRequest request) {
-        return ResponseEntity.ok(userService.createUser(request));
+    public ResponseEntity<GenericResponse<UserResponse>> createUser(@RequestBody CreateUserRequest request) {
+        return ResponseEntity.ok(GenericResponse.success(userService.createUser(request)));
     }
 
     @GetMapping("/email/{email}")
-    public ResponseEntity<UserResponse> getByEmail(@PathVariable String email) {
-        return ResponseEntity.ok(userService.getByEmail(email));
+    public ResponseEntity<GenericResponse<UserResponse>> getByEmail(@PathVariable String email) {
+        return ResponseEntity.ok(GenericResponse.success(userService.getByEmail(email)));
     }
 
     @GetMapping("/")
-    public ResponseEntity<List<UserResponse>> getAll() {
-        return ResponseEntity.ok(userService.getAll());
+    public ResponseEntity<GenericResponse<List<UserResponse>>> getAll() {
+        return ResponseEntity.ok(GenericResponse.success(userService.getAll()));
     }
 
     @GetMapping("/{userId}")
-    public ResponseEntity<UserResponse> getById(@PathVariable("userId") Long userId) {
-        return ResponseEntity.ok(userService.getById(userId));
+    public ResponseEntity<GenericResponse<UserResponse>> getById(@PathVariable("userId") Long userId) {
+        return ResponseEntity.ok(GenericResponse.success(userService.getById(userId)));
     }
 
     @GetMapping("/errorTest")
-    public ResponseEntity<List<UserResponse>> testKafka() {
+    public ResponseEntity<GenericResponse<List<UserResponse>>> testKafka() {
         throw new RuntimeException("test exception");
     }
 
