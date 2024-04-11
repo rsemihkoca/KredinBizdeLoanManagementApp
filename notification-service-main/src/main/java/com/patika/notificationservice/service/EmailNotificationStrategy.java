4c1efd59eb2dd3bbefa69c6b47f98cb49244a6fd
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-11 18:46:42 +0300

    feign client fix:notification-service-main/src/main/java/com/patika/notificationservice/service/EmailNotificationStrategy.java

--- notification-service-main/src/main/java/com/patika/notificationservice/service/EmailNotificationStrategy.java
+++ notification-service-main/src/main/java/com/patika/notificationservice/service/EmailNotificationStrategy.java
@@ -1,15 +1,62 @@
 package com.patika.notificationservice.service;
 
+import com.patika.notificationservice.dto.Notification;
+import com.patika.notificationservice.dto.enums.NotificationContent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 @Component
 @Slf4j
 @Scope(value = "prototype")
-public class EmailNotificationStrategy implements NotificationStrategy{
+public class EmailNotificationStrategy implements NotificationStrategy {
+
+
+    private static final String APPLICATION_CREATED_HTML;
+
+    static {
+        StringBuilder contentBuilder = new StringBuilder();
+        try {
+            BufferedReader in = new BufferedReader(new FileReader("templates/ApplicationCreated.html"));
+            String str;
+            while ((str = in.readLine()) != null) {
+                contentBuilder.append(str);
+            }
+            in.close();
+        } catch (IOException e) {
+        }
+        APPLICATION_CREATED_HTML = contentBuilder.toString();
+    }
+
+    private final EmailService emailService;
+
+    public EmailNotificationStrategy(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
     @Override
-    public void sendNotification(String message) {
+    public void sendNotification(Notification message) {
         log.info("Email gönderildi: {}", message);
+
+        switch (NotificationContent.valueOf(message.getMessage())) {
+            case APPLICATION_CREATED -> applicationCreated(message);
+            case APPLICATION_UPDATED -> log.info("Şifre sıfırlama maili gönderildi.");
+            case APPLICATION_DELETED -> log.info("Email bildirim maili gönderildi.");
+        }
+    }
+
+    private void applicationCreated(Notification message) {
+        log.info("Email doğrulama maili gönderildi.");
+        // html in templates/ApplciationCreated.html
+        try {
+            emailService.sendEmail(message.getChannel(), "Doğrulama maili", APPLICATION_CREATED_HTML);
+        }
+        catch (Exception e) {
+            log.error("Email gönderilemedi: {}", e.getMessage());
+        }
     }
 }
