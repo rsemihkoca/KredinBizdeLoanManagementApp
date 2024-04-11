21c0c00a4cffe41c1c752dea775a90fbb90ad010
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-11 18:46:42 +0300

    feign client fix:notification-service-main/src/main/java/com/patika/notificationservice/service/NotificationStrategy.java

--- notification-service-main/src/main/java/com/patika/notificationservice/service/NotificationStrategy.java
+++ notification-service-main/src/main/java/com/patika/notificationservice/service/NotificationStrategy.java
@@ -1,5 +1,7 @@
 package com.patika.notificationservice.service;
 
+import com.patika.notificationservice.dto.Notification;
+
 public interface NotificationStrategy {
-    void sendNotification(String message);
+    void sendNotification(Notification message);
 }
