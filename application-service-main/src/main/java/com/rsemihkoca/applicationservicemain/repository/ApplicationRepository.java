16bb819c2c25d68bc1d5d44d03d66818ff529a31
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-12 01:04:01 +0300

    user inactivation addded:application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/repository/ApplicationRepository.java

--- application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/repository/ApplicationRepository.java
+++ application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/repository/ApplicationRepository.java
@@ -2,9 +2,11 @@ package com.rsemihkoca.applicationservicemain.repository;
 
 import com.rsemihkoca.applicationservicemain.model.Application;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,6 +18,11 @@ public interface ApplicationRepository extends JpaRepository<Application, Long> 
 //
     @Query("SELECT a FROM Application a WHERE a.isActive = true")
     List<Application> findAllActiveApplications();
+
+//    @Transactional
+//    @Modifying
+//    @Query("UPDATE Application a SET a.isActive = false WHERE a.isActive = true AND a.userEmail = :email")
+//    void deactivateApplicationsByUserEmail(@Param("email") String userEmail);
 
 
 }
