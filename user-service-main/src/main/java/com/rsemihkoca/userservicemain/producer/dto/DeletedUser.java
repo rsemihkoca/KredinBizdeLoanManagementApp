84a3e5967130e00c38201fec65e4f9970a1682d3
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-12 01:04:01 +0300

    user inactivation addded:user-service-main/src/main/java/com/rsemihkoca/userservicemain/producer/dto/DeletedUser.java

--- user-service-main/src/main/java/com/rsemihkoca/userservicemain/producer/dto/DeletedUser.java
+++ user-service-main/src/main/java/com/rsemihkoca/userservicemain/producer/dto/DeletedUser.java
@@ -1,2 +1,14 @@
-package com.rsemihkoca.userservicemain.producer.dto;public class DeletedUser {
+package com.rsemihkoca.userservicemain.producer.dto;
+
+import lombok.*;
+import org.springframework.http.HttpStatus;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@EqualsAndHashCode
+public class DeletedUser {
+    private String email;
 }
