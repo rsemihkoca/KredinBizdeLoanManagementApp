73c186ac93862d38fce9f6ce5284c8f99f1dd93d
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-12 22:04:46 +0300

    send notification finished:application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/aspect/signature/SendNotification.java

--- application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/aspect/signature/SendNotification.java
+++ application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/aspect/signature/SendNotification.java
@@ -1,2 +1,18 @@
-package com.rsemihkoca.applicationservicemain.aspect.signature;public interface SendNotification {
+package com.rsemihkoca.applicationservicemain.aspect.signature;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+import com.rsemihkoca.applicationservicemain.enums.NotificationContent;
+import jakarta.validation.constraints.NotNull;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SendNotification{
+
+    @NotNull
+    NotificationContent content() ;
 }
