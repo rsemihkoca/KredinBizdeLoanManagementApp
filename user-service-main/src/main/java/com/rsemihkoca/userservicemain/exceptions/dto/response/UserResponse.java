532bd3f24815ee6e249abf633fd4011c1432143b
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-13 01:03:13 +0300

    user service test added:user-service-main/src/main/java/com/rsemihkoca/userservicemain/exceptions/dto/response/UserResponse.java

--- user-service-main/src/main/java/com/rsemihkoca/userservicemain/exceptions/dto/response/UserResponse.java
+++ user-service-main/src/main/java/com/rsemihkoca/userservicemain/exceptions/dto/response/UserResponse.java
@@ -1,9 +1,11 @@
 package com.rsemihkoca.userservicemain.exceptions.dto.response;
 
+import lombok.Builder;
 import lombok.Data;
 
 
 @Data
+@Builder
 public class UserResponse implements java.io.Serializable{
 
     private String name;
