ae309293ce6c16625de8ac384cf85e84403f0e6c
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-12 22:04:46 +0300

    send notification finished:application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/ApplicationServiceMainApplication.java

--- application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/ApplicationServiceMainApplication.java
+++ application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/ApplicationServiceMainApplication.java
@@ -5,11 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableCaching
+@EnableAspectJAutoProxy
+@ComponentScan("com.rsemihkoca")
+@SpringBootApplication
 public class ApplicationServiceMainApplication {
 
 	public static void main(String[] args) {
