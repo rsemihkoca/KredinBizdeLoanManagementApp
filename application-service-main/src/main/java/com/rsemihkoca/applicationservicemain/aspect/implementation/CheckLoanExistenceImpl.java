09d44ca17bb15b89cfa31896fa8f47f353105731
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-12 22:04:46 +0300

    send notification finished:application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/aspect/implementation/CheckLoanExistenceImpl.java

--- application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/aspect/implementation/CheckLoanExistenceImpl.java
+++ application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/aspect/implementation/CheckLoanExistenceImpl.java
@@ -1,16 +1,14 @@
 package com.rsemihkoca.applicationservicemain.aspect.implementation;
 
 import com.rsemihkoca.applicationservicemain.client.bankservice.BankServiceClient;
-import com.rsemihkoca.applicationservicemain.dto.request.interfaces.hasBankName;
-import com.rsemihkoca.applicationservicemain.dto.request.interfaces.hasLoanId;
+import com.rsemihkoca.applicationservicemain.dto.request.interfaces.HasBankName;
+import com.rsemihkoca.applicationservicemain.dto.request.interfaces.HasLoanId;
 import com.rsemihkoca.applicationservicemain.dto.response.GenericResponse;
 import com.rsemihkoca.applicationservicemain.dto.response.MergedLoanResponse;
 import com.rsemihkoca.applicationservicemain.model.ApplicationPipeline;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.core.annotation.Order;
@@ -23,7 +21,7 @@ import java.util.List;
 @Slf4j
 @AllArgsConstructor
 @Order(value = 2)
-public class CheckLoanExistence {
+public class CheckLoanExistenceImpl {
 
     private final BankServiceClient bankServiceClient;
     private final ApplicationPipeline applicationPipeline;
@@ -33,8 +31,8 @@ public class CheckLoanExistence {
         // get any bank name and Long loanId from the request
         Object[] args = joinPoint.getArgs();
         for (Object arg : args) {
-            if (arg instanceof hasLoanId && arg instanceof hasBankName) {
-                var request = (hasLoanId & hasBankName) arg;
+            if (arg instanceof HasLoanId && arg instanceof HasBankName) {
+                var request = (HasLoanId & HasBankName) arg;
                 String bankName = request.getBankName();
                 Long loanId = request.getLoanId();
                 findLoanByLoanIdAndBankName(loanId, bankName);
