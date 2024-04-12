ee22ee15085862369418b2d9ca714b48b60f4ec9
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-12 22:04:46 +0300

    send notification finished:application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/aspect/signature/CheckApplicationAbsence.java

--- application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/aspect/signature/CheckApplicationAbsence.java
+++ application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/aspect/signature/CheckApplicationAbsence.java
@@ -1,15 +1,13 @@
 package com.rsemihkoca.applicationservicemain.aspect.signature;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.annotation.Order;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.*;
+import java.lang.annotation.Target;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface CheckUserExistence{
+public @interface CheckApplicationAbsence {
+
 
 }
