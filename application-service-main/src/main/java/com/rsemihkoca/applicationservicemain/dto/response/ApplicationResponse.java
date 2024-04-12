bf2f1f4e4418b4800443743c918c0538fe46922a
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-12 22:04:46 +0300

    send notification finished:application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/dto/response/ApplicationResponse.java

--- application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/dto/response/ApplicationResponse.java
+++ application-service-main/src/main/java/com/rsemihkoca/applicationservicemain/dto/response/ApplicationResponse.java
@@ -1,18 +1,30 @@
 package com.rsemihkoca.applicationservicemain.dto.response;
 
+import com.rsemihkoca.applicationservicemain.dto.request.interfaces.CanSendNotification;
+import com.rsemihkoca.applicationservicemain.enums.NotificationContent;
+import com.rsemihkoca.applicationservicemain.enums.NotificationType;
+import com.rsemihkoca.applicationservicemain.producer.dto.Notification;
 import lombok.*;
 
 @Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class ApplicationResponse implements java.io.Serializable{
+public class ApplicationResponse implements java.io.Serializable, CanSendNotification {
 
     private String userEmail;
     private MergedLoanResponse mergedLoanResponse;
     private String applicationDate;
     private String applicationStatus;
 
+    @Override
+    public Notification getNotification(NotificationContent content) {
+        return Notification.builder()
+                .notificationType(NotificationType.EMAIL)
+                .channel(this.getUserEmail())
+                .message(content.toString())
+                .build();
+    }
 }
 
 
