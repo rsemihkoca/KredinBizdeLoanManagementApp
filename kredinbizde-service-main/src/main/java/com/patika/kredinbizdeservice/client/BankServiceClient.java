d7d6b74dd0c996088828a8f6a5a1014a1329e19c
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-13 17:02:57 +0300

    removed unnecessary folders and updated readme and documentation:kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/client/BankServiceClient.java

--- kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/client/BankServiceClient.java
+++
@@ -1,8 +0,0 @@
-package com.patika.kredinbizdeservice.client;
-
-import com.patika.kredinbizdeservice.client.dto.request.BankApplicationRequest;
-import com.patika.kredinbizdeservice.client.dto.response.ApplicationResponse;
-
-public interface BankServiceClient {
-    ApplicationResponse createApplication(BankApplicationRequest request);
-}
